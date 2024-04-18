52973e63d8844678ad82878057cc001742c00114
Pratik Suthar <psuthar1903@gmail.com>
2024-04-18 11:34:22 +0530

    pagination done

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -5,8 +5,27 @@ import React from "react";
 import { useState } from "react";
 import Navbar from "@/components/Navbar";
 export default function Issues(){
+
+    const data = [
+        {title : "1" , status : "open"},
+        {title : "2" , status : "open"},
+        {title : "3", status : "open"},
+        {title : "4" , status : "closed"},
+        {title : "5" , status : "closed"},
+        {title : "6", status : "in-progress"},
+        {title : "7", status : "closed"},
+        {title : "8" , status : "closed"},
+        {title : "9", status : "closed"},
+        {title : "10", status : "closed"},
+    ];
     const[open , setOpen] = useState(false)
     const [filterValue , setFilterValue] = useState('All')
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(7); // Number of items per page
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
     const onClickHandle = ()=>{
         setOpen(!open)
@@ -14,19 +33,23 @@ export default function Issues(){
 
     const filtervalueHandler = (value :any)=>{
         setFilterValue(value);
+        setOpen(!open)
     }
+
+    const paginate = (pageNumber :any) => setCurrentPage(pageNumber);
     return(
     
        <div>
          <Navbar/>
-       <div className=" flex flex-col justify-center m-10 align-center gap-5 relative">
+       <div className=" flex flex-col justify-center m-10 align-center gap-5 ">
          
-        <button className="w-max  p-2 pl-4 pr-4 rounded-lg text-[18px] flex flex-row gap-1 border-2 border-black " onClick={onClickHandle}>
+         <div className="flex ">
+        <button className="w-max  p-2 pl-4 pr-4 rounded-lg text-[18px] flex flex-row gap-1 border-2 border-black bg-black text-white  " onClick={onClickHandle}>
             <p>{filterValue}</p>
-            <VscTriangleUp className="text-center mt-1" />
+            <VscTriangleUp className="text-center mt-1 text-while"   />
         </button>
 
-        {open && <div className=" w-[10rem] bg-white  p-1 text-left border-2 border-black rounded-lg shadow-md shadow-gray-400 absolute  " > 
+        {open && <div className=" w-[10rem] bg-white  p-1 text-left border-[1px] border-gray rounded-lg shadow-md shadow-gray-400 absolute " > 
             <ul>
                 <li className=" p-2 pl-5 pr-5 hover:bg-black  hover:text-white rounded-lg mb-1 " 
                     onClick={()=>filtervalueHandler('All')}>All</li>
@@ -38,22 +61,57 @@ export default function Issues(){
                     onClick={()=>filtervalueHandler('closed')}>Closed</li>
             </ul>
         </div>}
+        </div>
 
         <div>
-        <div className="grid grid-cols-4 text-[18px] font-bold border-black border-2 rounded-t-lg bg-gray-100 ">
+        <div className="grid grid-cols-4 text-[18px] font-bold border-gray border-[1px] rounded-t-lg bg-gray-100 ">
           <div className="col-span-2 p-5 text-left "> issue</div>
           <div className="p-5 text-left ">status</div>
           <div className="p-5 text-left"> created</div>
         </div>
-        <Issue/>
-        <Issue/>
-        <Issue/>
-        <Issue/>
+        
+        {currentItems.map((item, index) => (
+          <Issue key={index} title={item.title} status={item.status}/>
+        ))}
+      
+        
         </div>
+
+        <div className="flex justify-start gap-5">
+        <button
+          className="bg-black text-white w-[6rem] p-3 rounded-lg "
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <button
+          className="bg-black text-white w-[6rem] p-3 rounded-lg "
+          onClick={() => paginate(currentPage + 1)}
+          disabled={indexOfLastItem >= data.length}
+        >
+          Next
+        </button>
+      </div>
 
         
        </div>
 
+
+       {/* <div>
+        <button
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => paginate(currentPage + 1)}
+          disabled={indexOfLastItem >= data.length}
+        >
+          Next
+        </button>
+      </div> */}
       
        </div>
     )
--- src/components/Issue.tsx
+++ src/components/Issue.tsx
@@ -1,12 +1,30 @@
-export default function Issue(){
+import { useState } from "react";
+
+interface issueProps{
+    title : string,
+    status : string
+}
+const  Issue:React.FC<issueProps> =(props) =>{
+
+    // const[closed , setClosed] = useState(false);
+
+    // if(props.status === 'closed'){
+    //     setClosed(true);
+    // }
+    
     return(
-       <div className="grid grid-cols-4 text-[18px] border-b-2 border-black border-r-2 border-l-2 ">
-          <div className="col-span-2 p-2 text-left ml-2"> issue</div>
-          {/* <div className="p-5 text-center"><p className="bg-red-200 text-red-700 font-bold text-[14px] w-max pl-3 pr-3 pt-2 pb-2 rounded-lg">open</p></div> */}
-          {/* <div className="p-5 text-center"><p className="bg-green-200 text-green-700 font-bold text-[14px] w-max pl-3 pr-3 pt-2 pb-2 rounded-lg">closed</p></div> */}
-          <div className="p-2 text-left"><p className="bg-violet-200 text-violet-700 font-bold text-[14px] w-max pl-3 pr-3 pt-2 pb-2 rounded-lg">in-progress</p></div>
+       <div className="grid grid-cols-4 text-[18px] border-b-[1px] border-gray border-r-[1px] border-l-[1px] ">
+          <div className="col-span-2 p-2 text-left ml-2"> {props.title}</div>
+
+          {props.status === 'closed' && <div className="p-3 text-center"><p className="bg-red-200 text-red-700 font-bold text-[14px] w-max pl-3 pr-3 pt-1 pb-1 rounded-lg">open</p></div>}
+
+          {props.status === 'open' && <div className="p-3 text-center"><p className="bg-green-200 text-green-700 font-bold text-[14px] w-max pl-3 pr-3 pt-1 pb-1 rounded-lg">closed</p></div> }
+          
+          {props.status === 'in-progress' &&<div className="p-3 text-left"><p className="bg-violet-200 text-violet-700 font-bold text-[14px] w-max pl-3 pr-3 pt-1 pb-1 rounded-lg">{props.status}</p></div>}
           <div className="p-2 text-left"> sat 12 2003</div>
 
        </div>
     )
 }
+
+export default Issue;
